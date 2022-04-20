4eaced28c7011ffc6eb65cb73729d6dba2b423b0
primavista40 <primavista40@gmail.com>
2022-04-20 16:14:04 +0700

    ddd

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code>, test and save to reload.
+          Edit <code>src/App.js</code>, test and save to 4/10/2022
         </p>
         <a
           className="App-link"
